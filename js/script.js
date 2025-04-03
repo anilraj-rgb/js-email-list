8377c28c09906bc508a75895d1f874ff8b7833f6
anilraj-rgb <anil.raj9727@gmail.com>
2025-04-03 16:08:23 +0200

    [UPD] put the const

---
+++ js/script.js
@@ -0,0 +1,3 @@
+const requestTarget = 'https://flynn.boolean.careers/exercises/api/random/mail'
+const emailContainer = document.getElementById('email-container')
+const button = document.getElementById('generate')
